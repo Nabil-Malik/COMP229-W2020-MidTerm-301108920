8539149e4508306ec8d5bb4f12442dc4d9ca9188
Nabil Malik <nabilmalik037@icloud.com>
2020-10-30 08:00:29 -0400

    After adding functionalities

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  
+  "URI": "mongodb+srv://nabilmaliik:azekmi@cluster0.o4n4f.mongodb.net/COMP229-W2020-MidTerm-301108920?retryWrites=true&w=majority"
 };
